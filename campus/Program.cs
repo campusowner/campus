b03a37adf4978084bdef03b83f627623348e664b
Umut Caner <uumutcanerr@gmail.com>
2024-12-07 16:34:38 +0300

    umut

--- campus/Program.cs
+++ campus/Program.cs
@@ -8,7 +8,7 @@ namespace campus
         [STAThread]
         static void Main()
         {
-            // ebeni si
+            // ebeni siqjhfbqýb
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
